5516807198b716f496c02e7d33f29085479cd1b3
Sungpil An <ansungpil1@gmail.com>
2021-01-25 15:25:50 -0500

    Added orm object

--- config/orm.js
+++ config/orm.js
@@ -22,8 +22,73 @@ function objToSql(ob) {
         if (Object.hasOwnProperty.call(ob, key)) {
             // if string is with spaces, add quotations
             if (typeof value === "string" && value.indexOf(" ") >= 0) {
-                value = "'" + value + "'"
+                value = "'" + value + "'";
+            };
+            arr.push(key + "=" + value);
+        };
+    };
+    return arr.toString();
+};
+
+// Object for all our SQL statement functions
+let orm = {
+    // All
+    all: function(tableInput, cb) {
+        let queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
             }
-        }
-    }
-}
+            cb(result);
+        });
+    },
+    // Create
+    create: function(table, cols, vals, cb) {
+        let queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString)
+
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    // Update
+    update: function(table, objColVals, condition, cb) {
+        let queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+        
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    // Delete
+    delete: function(table, id, cb) {
+        connection.query("DELETE FROM ?? WHERE is = ?", [table,id], function(err, result) {
+            if (err) throw err;
+            cb(result);
+        });
+    };
+};
+
+// Export the orm object for the model
+module.exports = orm
