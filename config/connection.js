11f687eb39144e27f72d7426c33f99464a7fc1a4
Sungpil An <ansungpil1@gmail.com>
2021-01-26 15:06:26 -0500

    init

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Set up MySQL connection.
 // const express = require("express");
 var mysql = require("mysql");
-
+let connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
